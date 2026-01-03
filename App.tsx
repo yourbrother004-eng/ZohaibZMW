f5d26c08e5fb7e23fd867860d9d9f3d19da3f4a1
yourbrother004-eng <yourbrother004@gmail.com>
2026-01-03 19:56:04 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import TechnicalSkills from './components/TechnicalSkills';
